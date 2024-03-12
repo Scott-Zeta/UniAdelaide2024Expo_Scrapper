69b682b43355ac236629bd207d7cf9422e7c5df4
Scott Zeta <scott-zeta@outlook.com>
2024-03-12 14:15:25 +1030

    clean the header footer and other no need elements

--- scrapper.py
+++ scrapper.py
@@ -11,6 +11,10 @@ def scrape(url):
         page.raise_for_status()  # Raises an HTTPError for bad responses
         soup = BeautifulSoup(page.content, "html.parser")
         
+        # clean the elements not needed
+        for unwanted_section in soup.select('header, footer, nav, .sidebar'):
+            unwanted_section.decompose()
+        
         main_content = soup.find("main")
         if main_content is None:
             logging.warning(f"No <main> element found. For {url}")
