6a5235f69e405f3740f2a1bfa8f503fb09a6644d
Scott Zeta <scott-zeta@outlook.com>
2024-03-13 02:52:13 +1030

    set a time sleep for Optiver scrapper defends

--- scrapper.py
+++ scrapper.py
@@ -1,9 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import logging
+import time
 
 def scrape(url):
     try:
+        # This is to avoid being blocked by the Optiver 429 too many requests
+        # Can be implemented as a backoff strategy if needed
+        time.sleep(0.1) 
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
         }
